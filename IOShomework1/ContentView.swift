08b05e0f7aedf66d51f14e3817ba388cb76126be
Evan <evan20010126@gmail.com>
2021-10-21 01:33:19 +0800

    Initial Commit

---
+++ IOShomework1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOShomework1
+//
+//  Created by Evan on 2021/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOShomework1/IOShomework1App.swift
@@ -0,0 +1,17 @@
+//
+//  IOShomework1App.swift
+//  IOShomework1
+//
+//  Created by Evan on 2021/10/21.
+//
+
+import SwiftUI
+
+@main
+struct IOShomework1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
